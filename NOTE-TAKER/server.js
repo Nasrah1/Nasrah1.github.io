79a7aa9a7578fcb378829c1745bff27271197f1f
“Nasra <nasra_09@yahoo.com>
2020-09-26 22:46:53 -0500

    My commit changes

--- NOTE-TAKER/server.js
+++ NOTE-TAKER/server.js
@@ -1,71 +1,69 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
 var path = require("path");
+// Defines a port to listen for requests
 var PORT = process.env.PORT || 3001;
 var fs = require("fs");
+let db = require("./db/db.json");
 
-var rootObj = { root: __dirname };
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "/public")));
-app.get("/*", (req, res) => {
-    res.sendFile("/public/index.html", rootObj);
+// Sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Get Methods
+app.get("/assets/js/index.js"),
+    function(req, res) {
+        res.sendFile(path.join(__dirname, "public/assets/index.js"));
+    };
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
-app.get("/notes", (req, res) => {
-    res.sendFile("/public/notes.html", rootObj);
+
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
 });
-app.get("/api/notes", (req, res) => {
-    console.log("/api/notesget");
-    let json = getJson();
-    console.log(json);
-    res.json(json);
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
-app.post("/api/notes", (req, res) => {
-    console.log("/api/notes/:");
-    console.log(req.body);
-    addNoteToJSON(req.body);
-    res.json(getJson());
+
+let notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
+        if (err) {
+            throw err;
+        }
+    })
+);
+
+var writeNewNote = function(note) {
+    fs.writeFileSync(
+        path.join(__dirname, "/db/db.json"),
+        JSON.stringify(note),
+        (err, data) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+};
+
+//Post Method here
+app.post("/api/notes", function(req, res) {
+    // Receives a new note, adds it to db.json, then returns the new note
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log("Added new note: " + newNote.title);
 });
-app.delete("/api/notes/:id", (req, res) => {
-    console.log("/api/notes/:iddelete");
-    deleteNoteFromJSON(req.params.id);
-    res.json(getJson());
+
+app.delete("/api/notes/:id", function(req, res) {
+    notes.splice(req.params.id, 1);
+    updateDb();
+    console.log("Deleted note with id " + req.params.id);
 });
 
 app.listen(PORT, function() {
     console.log("Server is ready to listen on: http://localhost:" + PORT);
 });
-
-function getJson() {
-    let data = fs.readFileSync(__dirname + "/db/db.json");
-    let json = JSON.parse(data);
-    return json;
-}
-
-function createNoteObject(data) {
-    let obj = {
-        title: data.title,
-        text: data.text,
-        complete: false,
-        hidden: false,
-    };
-    return obj;
-}
-
-function addNoteToJSON(note) {
-    let json = getJson();
-    let newNote = createNoteobject(note);
-    json.push(newNote);
-    saveJSON(json);
-}
-
-function saveJSON(jsonData) {
-    let data = JSON.stringify(jsonData);
-    fs.writeFileSync(__dirname + "/db/db.json", data);
-}
-
-function deleteFileFromJSON(id) {
-    let json = getJson();
-    json[id].hide = true;
-    saveJSON(json);
-}
