fd790d8128ab25f3da568beba9d749d49393c4de
“Nasra <nasra_09@yahoo.com>
2020-10-11 22:14:25 -0500

    Updated README

--- BurgerApp/server.js
+++ BurgerApp/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Import routes and give the server access to them.
+/// Import routes and give the server access to them.
 const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 app.listen(PORT, function() {
