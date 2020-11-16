626247ca900c218d71e733e9f24dcf860d49a055
“Nasra <nasra_09@yahoo.com>
2020-11-16 11:18:26 -0600

    Updated Apllication

--- NOTE-TAKER/README.md
+++ NOTE-TAKER/README.md
@@ -1,1 +1,29 @@
-In this application you'll find a db.json file on the backend that will be used to store and retrieve notes using the fs module. Three API GET,POST,DELETE routes was created.
+# Note Taker App
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Technologies Used](#technologies-used)
+- [Demo](#demo)
+
+## Description
+
+In this application you'll find a db.json file on the backend that will be used to store and retrieve notes using the fs module. API routes GET,POST,DELETE to help the apllication run smooth.
+
+## Installation
+
+To access the application run node server.js and open broweser go to http://localhost:3000 in your preferred browser.
+
+## Technologies Used
+
+- Node.js
+- Express
+- Javascript
+- CSS
+- HTML
+
+## Demo
+
+![Note-Taker](assets/img/Note-Taker.gif)
+
--- NOTE-TAKER/server.js
+++ NOTE-TAKER/server.js
@@ -1,70 +1,22 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-// Defines a port to listen for requests
-var PORT = process.env.PORT || 3001;
-var fs = require("fs");
-let db = require("./db/db.json");
+// Dependencies
+const express = require("express");
+const fs = require("fs");
 
-// Sets up express app to handle data parsing
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000 
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/assets", express.static("./assets"));
 
-//Get Methods
-app.get("/assets/js/index.js"),
-    function(req, res) {
-        res.sendFile(path.join(__dirname, "public/assets/index.js"));
-    };
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
-app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-});
-
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-let notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
-        if (err) {
-            throw err;
-        }
-    })
-);
-
-var writeNewNote = function(note) {
-    fs.writeFileSync(
-        path.join(__dirname, "/db/db.json"),
-        JSON.stringify(note),
-        (err, data) => {
-            if (err) {
-                throw err;
-            }
-        }
-    );
-};
-
-//Post Method here
-app.post("/api/notes", function(req, res) {
-    // Receives a new note, adds it to db.json, then returns the new note
-    let newNote = req.body;
-    console.log(newnote);
-    alert("heretoo");
-    notes.push(newNote);
-    return console.log("Added new note: " + newNote.title);
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    console.log(req.params);
-    notes.pop(req.params.id, 1);
-    console.log("Deleted note with id " + req.params.id);
-});
-
+// Starts the server to begin listening
 app.listen(PORT, function() {
-    console.log("Server is ready to listen on: http://localhost:" + PORT);
+    console.log("App listening on PORT " + PORT);
 });
